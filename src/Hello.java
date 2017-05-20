bd448e238ceb20ed91c12bd22aee59a52212b208
Avinash Pandey <avinashie@hotmail.com>
2017-05-20 22:56:00 +1000

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 5/14/2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello! Git Mate");
+    }
+
+}
